7836156bac0d46dc8b9f7969ba32aed991e03e24
mctammer <tmmohamed@ucdavis.edu>
2020-05-14 20:44:58 -0700

    Match user input to a hard-coded phrase.

--- project/button.js
+++ project/button.js
@@ -1,5 +1,25 @@
 ﻿
 function getInput() {
 	var myInput = document.getElementById("word").value;
-	alert("Your word is " + myInput)
+	//alert("Your word is " + myInput)
+	matchInput();
+  }
+ function matchInput() {
+
+	let userInput = document.getElementById("word").value;
+	let neededInput = "FreDerick!";
+	let oglength = neededInput.length;
+	let uiLength = userInput.length;
+	if(uiLength != oglength) {
+		alert("You entered the wrong phrase, you might be a phony!!");
+	}
+	else {
+		let result = userInput.match(/FreDerick!/g);
+		if(!result) {
+			alert("You entered the wrong phrase, you might be a phony!!");
+		}
+		else {
+			alert("You entered the correct phrase!");
+		}
+	}
   }
